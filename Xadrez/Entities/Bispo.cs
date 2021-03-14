6af4c4efc2c31a5643de42efd263f4a2e369a748
Gustavo Zotarelli <gustavozotarelli@gmail.com>
2021-03-14 11:19:26 -0300

    Implementada as movimentações das peças - V11

--- Xadrez/Entities/Bispo.cs
+++ Xadrez/Entities/Bispo.cs
@@ -12,9 +12,71 @@ namespace Xadrez.Entities
 
         }
 
+        private bool PodeMover(Posicao pos)
+        {
+            Peca p = Tab.Peca(pos);
+            return p == null || p.Cor != Cor;
+        }
+
         public override bool[,] MovimentosPossiveis()
         {
-            throw new NotImplementedException();
+            bool[,] mat = new bool[Tab.Linhas, Tab.Colunas];
+
+            Posicao pos = new Posicao(0, 0);
+
+            //Nordeste
+            pos.DefinirValores(Posicao.Linha - 1, Posicao.Coluna + 1);
+            while (Tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+                if (Tab.Peca(pos) != null && Tab.Peca(pos).Cor != Cor)
+                {
+                    break;
+                }
+                pos.Linha -= 1;
+                pos.Coluna += 1;
+            }
+
+            //Sudeste
+            pos.DefinirValores(Posicao.Linha + 1, Posicao.Coluna + 1);
+            while (Tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+                if (Tab.Peca(pos) != null && Tab.Peca(pos).Cor != Cor)
+                {
+                    break;
+                }
+                pos.Linha += 1;
+                pos.Coluna += 1;
+            }
+
+            //Sudoeste
+            pos.DefinirValores(Posicao.Linha + 1, Posicao.Coluna - 1);
+            while (Tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+                if (Tab.Peca(pos) != null && Tab.Peca(pos).Cor != Cor)
+                {
+                    break;
+                }
+                pos.Linha += 1;
+                pos.Coluna -= 1;
+            }
+
+            //Noroeste
+            pos.DefinirValores(Posicao.Linha - 1, Posicao.Coluna - 1);
+            while (Tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+                if (Tab.Peca(pos) != null && Tab.Peca(pos).Cor != Cor)
+                {
+                    break;
+                }
+                pos.Linha -= 1;
+                pos.Coluna -= 1;
+            }
+
+            return mat;
         }
 
         public override string ToString()
--- Xadrez/Entities/Cavalo.cs
+++ Xadrez/Entities/Cavalo.cs
@@ -12,9 +12,74 @@ namespace Xadrez.Entities
 
         }
 
+        private bool PodeMover(Posicao pos)
+        {
+            Peca p = Tab.Peca(pos);
+            return p == null || p.Cor != Cor;
+        }
+
         public override bool[,] MovimentosPossiveis()
         {
-            throw new NotImplementedException();
+            bool[,] mat = new bool[Tab.Linhas, Tab.Colunas];
+
+            Posicao pos = new Posicao(0, 0);
+
+            //Acima/Esquerda
+            pos.DefinirValores(Posicao.Linha - 2, Posicao.Coluna - 1);
+            if (Tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+
+            //Acima/Direita
+            pos.DefinirValores(Posicao.Linha - 2, Posicao.Coluna + 1);
+            if (Tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+
+            //Baixo/Direita
+            pos.DefinirValores(Posicao.Linha + 2, Posicao.Coluna + 1);
+            if (Tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+
+            //Baixo/Esquerda
+            pos.DefinirValores(Posicao.Linha + 2, Posicao.Coluna - 1);
+            if (Tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+
+            //Esquerda/Acima
+            pos.DefinirValores(Posicao.Linha - 1, Posicao.Coluna - 2);
+            if (Tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+
+            //Direita/Acima
+            pos.DefinirValores(Posicao.Linha - 1, Posicao.Coluna + 2);
+            if (Tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+
+            //Direita/Baixo
+            pos.DefinirValores(Posicao.Linha + 1, Posicao.Coluna + 2);
+            if (Tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+
+            //Esquerda/Baixo
+            pos.DefinirValores(Posicao.Linha + 1, Posicao.Coluna - 2);
+            if (Tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+            return mat;
         }
 
         public override string ToString()
